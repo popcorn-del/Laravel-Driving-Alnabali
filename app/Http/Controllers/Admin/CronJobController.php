159443856d0d960b7afcf2aee01174a0cad5af4d
Bilal Riaz <44498738+BilalRiaz1@users.noreply.github.com>
2023-03-26 20:34:14 +0500

    add cron job

---
+++ app/Http/Controllers/Admin/CronJobController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class CronJobController extends Controller
+{
+    public function index(){
+        return view('admin.pages.cronjob.index');
+    }
+    public function start(){
+        Artisan::call('day:update');
+        Artisan::call('five:update');
+        return redirect()->back()->with('success','CronJob run successfully');;
+    }
+}
---
+++ resources/views/admin/pages/cronjob/index.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.layouts.master')
+@section('title') Cronjob @endsection
+@section('page-title') {{__('cronjob')}} @endsection
+
+@section('css')
+@endsection
+@section('content')
+    <div class="content-warpper">
+        <div class="row">
+            <div class="col-12">
+                <!--Bus List section -->
+                <div class="card">
+                    <div class="card-body">
+                        <div class="text-center">
+                            @if (Session::has('success'))
+                                <div class="alert alert-success">
+                                    {{Session::get('success')}}
+                                </div>
+                            @endif
+                            <form action="{{route('cronjob.start')}}" method="post">
+                                @csrf
+                                <button class="btn btn-success">Run Cronjob</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+    </div>
+@endsection
+
